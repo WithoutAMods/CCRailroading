a80abbdce1e364ae447b0753bdf5f0e76be38c86
WithoutAName <withoutaname@withoutaname.eu>
2026-08-11 20:08:47 +0200

    NeoForge Mod Generator Template

---
+++ src/main/java/eu/withoutaname/mods/cc_railroading/CCRailroadingClient.java
@@ -0,0 +1,31 @@
+package eu.withoutaname.mods.cc_railroading;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = CCRailroading.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = CCRailroading.MODID, value = Dist.CLIENT)
+public class CCRailroadingClient {
+    public CCRailroadingClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        CCRailroading.LOGGER.info("HELLO FROM CLIENT SETUP");
+        CCRailroading.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
